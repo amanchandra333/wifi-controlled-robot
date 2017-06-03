42ee4ae68a1db4bb337cd10e2e09ace0714a5fc7
amanchandra333 <amanchandra333@gmail.com>
2017-06-04 02:23:11 +0530

    add python script

---
+++ client.py
@@ -0,0 +1,50 @@
+import time
+import sys
+import socket
+from threading import *
+
+connected = False
+# Set up socket connection for recieving and sending data to the server
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#Bind to any incoming request at port 8000
+host = "0.0.0.0"
+port = 8000
+print (host)
+print (port)
+serversocket.bind((host, port))
+#Thread class to recieve data and implement methods
+class client(Thread):
+    def __init__(self, socket, address):
+ 	Thread.__init__(self)
+        self.sock = socket
+        self.addr = address
+        self.start()
+
+    def run(self):
+	print "connected"
+        connected = True
+        while 1:
+ 	    pos = self.sock.recv(1024)
+            if pos == 'F':
+                print "recieved F"
+                #do your stuff here
+            elif pos == 'B':
+                print "recieved B"
+                #...
+            else :
+                print "blah blah blah"
+                #...
+            self.sock.send("recieved"+'\n')
+	    time.sleep(1)
+
+def main():
+    serversocket.listen(5)
+    print ('server started and listening')
+    while not connected:
+        clientsocket, address = serversocket.accept()
+        client(clientsocket, address)
+	
+    while connected:
+        time.sleep(3)
+
+main()
